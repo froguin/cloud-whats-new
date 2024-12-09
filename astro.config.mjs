43f2c195bd94013583f05ac34e09b57f59f53eba
Kim, SeongHan <froguin3@gmail.com>
2024-12-10 06:50:34 +0900

    modify astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
-  integrations: [tailwind(), react()],
+  adapter: netlify()
 });
